552ad225b692221e76986c94c5db8eb8bc8c58a8
zin071469-eng <zin071469@gmail.com>
2026-09-26 07:14:13 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ async def song(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'sleep_interval': 3,
         'max_sleep_interval': 6,
         'sleep_requests': 1,
-        'cookiefile': 'cookies.txt',
+     
     }
 
         
